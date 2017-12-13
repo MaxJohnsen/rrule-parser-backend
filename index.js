8afbc95800ca56cb84e8f1bc6e0402e9884beb47
Max Aasbø <max@dira.no>
2017-12-13 15:28:06 +0100

    Setup root route and extend readme

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+  res.send(`<div>
+      <h1>Hello World!</h1>
+      <p>Please send post requests to <a href="https://rrule-parser.herokuapp.com/parse">https://rrule-parser.herokuapp.com/parse</a>.</p>
+      <a href="https://github.com/MaxJohnsen/rrule-parser-backend">Github</a>
+    </div>`);
+});
+
 app.post('/parse/', (req, res) => {
   try {
     const { rrule, fromDate, startAfter } = req.body;
